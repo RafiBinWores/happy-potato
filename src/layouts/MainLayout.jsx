122628de431c74f5a45a777cf00781fadc9f783c
Rafi Bin Wores <rafibinwores2203@gmail.com>
2025-11-03 18:46:10 +0600

    Layout file import typo fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Navbar from "../components/navbar/Navbar";
 import FixedBg from "../components/fixedBg/FixedBg";
 import Footer from "../components/footer/Footer";
-import RootSEO from "../components/seo/RootSeo";
+import RootSEO from "../components/seo/RootSEO";
 
 const MainLayout = () => {
   return (
